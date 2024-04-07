61f32ac55a8aaed28b72e0f1261b19876aa83672
doah36 <doah.kim@u.nus.edu>
2024-04-07 20:00:49 +0800

    App2

---
+++ personal_app/token.R
@@ -0,0 +1,3 @@
+rsconnect::setAccountInfo(name='doah240315',
+                          token='BD1083B78040306A2B8F15E005AE2974',
+                          secret='JN50stooB+VqmZtvklJWTKEY4/irp8yxlKH9G735')
